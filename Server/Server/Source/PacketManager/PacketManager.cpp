7242ee8ee3c611be0486b635d1445c9ec9b8216a
NERU <32727064+haein0303@users.noreply.github.com>
2024-07-20 15:07:20 +0900

    패킷 프로세싱 예외처리 반응 코드 추가
    패킷 처리 중 에러가 발생할 경우의 처리 추가

--- Server/Server/Source/PacketManager/PacketManager.cpp
+++ Server/Server/Source/PacketManager/PacketManager.cpp
@@ -1,5 +1,7 @@
 #include "PacketManager.h"
 #include "PacketProcessors/Processors.h"
+#include <sstream>
+#include <exception>
 
 void PacketManager::Init(Server* server)
 {
@@ -13,6 +15,29 @@ void PacketManager::Init(Server* server)
 void PacketManager::ProcessPacket(const int type, const uint8_t* data, const int size, const int key)
 {
 	PacketProcessorMap[type]->Process(pServer, data, size, key);
+
+	//try {
+ //       PacketProcessorMap[type]->Process(pServer, data, size, key);
+	//}
+	//catch (const std::exception& e) {
+ //       std::cerr << "[ERROR] : " << e.what() << " Type : " << type << " KEY : " << key << std::endl;
+
+ //       // 호출 스택 추적
+ //       std::stringstream ss;
+ //       ss << e.what();
+
+ //       // 예외 메시지에서 파일 이름과 라인 번호 추출
+ //       std::string line;
+ //       while (getline(ss, line)) {
+ //           if (line.find(":") != std::string::npos) {
+ //               size_t pos = line.find(":");
+ //               std::string file = line.substr(0, pos);
+ //               int lineNum = stoi(line.substr(pos + 1));
+
+ //               std::cerr << "  " << file << ":" << lineNum << std::endl;
+ //           }
+ //       }
+	//}
 }
 
 void PacketManager::ProcessEvent(const int type, const uint8_t* data, const int size)
