70ddb1517bda832a26431cae614fe0e73dd2f61d
DorongSeok <71745818+DorongSeok@users.noreply.github.com>
2024-07-21 18:36:34 +0900

    Client: Add Player Camera Controller

---
+++ Client/Assets/02. Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public Transform cameraArm;
+    public Transform root;
+
+    private float mouseX, mouseY;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        LookAround();
+    }
+
+    private void Move()
+    {
+        cameraArm.position = new Vector3(root.position.x, 0, root.position.z);
+    }
+    private void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X") * rotationSpeed, Input.GetAxis("Mouse Y") * rotationSpeed);
+        Vector3 cameraAngle = cameraArm.rotation.eulerAngles;
+        float x = cameraAngle.x - mouseDelta.y;
+
+        if (x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 20f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 330f, 361f);
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, cameraAngle.y + mouseDelta.x, cameraAngle.z);
+    }
+}
