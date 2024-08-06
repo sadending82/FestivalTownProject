47a736ab2e592d06b769d5f20367c22a865d3969
DorongSeok <71745818+DorongSeok@users.noreply.github.com>
2024-08-06 16:04:48 +0900

    Client: Fix Add Player
    활성화 되어 있는 플레이어가 이미 활성화 되어 있는지 확인하는 기능과 디버그 로그들 추가

--- Client/Assets/02. Scripts/Network/PacketProcessor/PlayerAdd.cs
+++ Client/Assets/02. Scripts/Network/PacketProcessor/PlayerAdd.cs
@@ -18,8 +18,6 @@ namespace Network.PacketProcessor
 
             var Data = PlayerAdd.GetRootAsPlayerAdd(bb);
 
-            Debug.Log("PlayerAdd! " + Data.Id);
-
             //int id = Data.Id;
             //Vector3 pos = new Vector3(Data.Pos.Value.X, Data.Pos.Value.Y, Data.Pos.Value.Z);
             //Vector3 dir = new Vector3(Data.Direction.Value.X, Data.Direction.Value.Y, Data.Direction.Value.Z);
