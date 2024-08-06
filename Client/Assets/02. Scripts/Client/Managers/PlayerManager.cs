8b141fbb5d94690d77aa49df27cbb858de185eaf
DorongSeok <71745818+DorongSeok@users.noreply.github.com>
2024-08-06 14:50:13 +0900

    Client: Modify Add Player
    플레이어를 추가하는 방식을 모든 플레이어가 접속했을 때 한 번에 생성하는 방식에서 플레이어가 접속할 때마다 생성하는 방식으로 변경

--- Client/Assets/02. Scripts/Client/Managers/PlayerManager.cs
+++ Client/Assets/02. Scripts/Client/Managers/PlayerManager.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     private int myId;
-    private int maxPlayer = 2;
+    private int maxPlayerNum = 6;
+    private int nowPlayerNum;
+    private bool createMyCharacter;
 
-    public void Start()
+    private void Awake()
+    {
+        nowPlayerNum = 0;
+        createMyCharacter = false;
+    }
+
+    private void Start()
     {
         //----- Client Test -----
         //SetMyId(0);
-        //AddPlayers();
+        //AddPlayers(2);
     }   
     public void SetMyId(int myId)
     {
         this.myId = myId;
+        nowPlayerNum = myId + 1;
     }
 
     public int GetMyId()
@@ -23,13 +32,46 @@ public class PlayerManager : MonoBehaviour
         return this.myId;
     }
 
-    public void AddPlayers()
+    public void AddPlayer()
     {
-        for (int i = 0; i < maxPlayer; ++i)
+        // 내 캐릭터를 만든 상태인지 체크, 내가 접속하기 전에 접속한 플레이어와 내 캐릭터를 생성
+        if (createMyCharacter == false)
+        {
+            for (int i = 0; i < nowPlayerNum; ++i)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                if (myId == i)
+                {
+                    createMyCharacter = true;
+                    transform.GetChild(i).gameObject.GetComponent<CharacterStatus>().SetAmIPlayer(true);
+                    transform.GetChild(i).gameObject.GetComponent<PlayerController>().SetMyId(myId);
+                }
+            }
+        }
+        // 내가 접속한 후에 새로운 플레이어가 접속했을때 실행
+        else
+        {
+            if (nowPlayerNum + 1 > maxPlayerNum)
+            {
+                Debug.Log("Error!!! : You Can't Add Player, Maximum Number of Players");
+            }
+            else
+            {
+                transform.GetChild(nowPlayerNum).gameObject.SetActive(true);
+                nowPlayerNum++;
+            }
+        }
+    }
+
+    // 클라이언트 테스트용
+    private void AddPlayers(int maxPlayerNum)
+    {
+        for (int i = 0; i < maxPlayerNum; ++i)
         {
             transform.GetChild(i).gameObject.SetActive(true);
-            if(myId == i)
+            if (myId == i)
             {
+                createMyCharacter = true;
                 transform.GetChild(i).gameObject.GetComponent<CharacterStatus>().SetAmIPlayer(true);
                 transform.GetChild(i).gameObject.GetComponent<PlayerController>().SetMyId(myId);
             }
