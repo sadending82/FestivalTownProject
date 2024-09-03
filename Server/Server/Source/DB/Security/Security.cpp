fe60b50b7f97917ed86b48a18363ca745e5424ca
SeongJae Han <anis15740@gmail.com>
2024-09-03 18:18:19 +0900

    OpenSSL 삭제
    설치 문제땜에 삭제. sha-256사용하기 위한 다른 방법 찾아야함

--- Server/Server/Source/DB/Security/Security.cpp
+++ Server/Server/Source/DB/Security/Security.cpp
@@ -1,50 +1,50 @@
-#pragma once
-#include "Security.h"
-
-std::string Security::GenerateSalt()
-{
-	unsigned char salt[SALT_LENGTH];
-	if (RAND_bytes(salt, SALT_LENGTH) == false) {
-		DEBUGMSGNOPARAM("Salt Generate Failed\n");
-	}
-
-	std::string hexSalt;
-
-	for (int i = 0; i < SALT_LENGTH; ++i) {
-		char hex[3];
-		sprintf_s(hex, "%02x", salt[i]);
-		hexSalt += hex;
-	}
-
-	return hexSalt;
-}
-
-std::string Security::HashingPassword(const std::string password, const std::string salt)
-{
-	std::string str = password + salt;
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-
-	SHA256_CTX sha256;
-	SHA256_Init(&sha256);
-	SHA256_Update(&sha256, str.c_str(), str.size());
-	SHA256_Final(hash, &sha256);
-
-	std::string hashedStr;
-
-	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-		char hex[3];
-		sprintf_s(hex, "%02x", hash[i]);
-		hashedStr += hex;
-	}
-
-	return hashedStr;
-}
-
-bool Security::VerifyPassword(std::string password, std::string hashedPassword, std::string salt)
-{
-	if (hashedPassword != HashingPassword(password, salt)) {
-		return false;
-	}
-
-	return true;
-}
+//#pragma once
+//#include "Security.h"
+//
+//std::string Security::GenerateSalt()
+//{
+//	unsigned char salt[SALT_LENGTH];
+//	if (RAND_bytes(salt, SALT_LENGTH) == false) {
+//		DEBUGMSGNOPARAM("Salt Generate Failed\n");
+//	}
+//
+//	std::string hexSalt;
+//
+//	for (int i = 0; i < SALT_LENGTH; ++i) {
+//		char hex[3];
+//		sprintf_s(hex, "%02x", salt[i]);
+//		hexSalt += hex;
+//	}
+//
+//	return hexSalt;
+//}
+//
+//std::string Security::HashingPassword(const std::string password, const std::string salt)
+//{
+//	std::string str = password + salt;
+//	unsigned char hash[SHA256_DIGEST_LENGTH];
+//
+//	SHA256_CTX sha256;
+//	SHA256_Init(&sha256);
+//	SHA256_Update(&sha256, str.c_str(), str.size());
+//	SHA256_Final(hash, &sha256);
+//
+//	std::string hashedStr;
+//
+//	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+//		char hex[3];
+//		sprintf_s(hex, "%02x", hash[i]);
+//		hashedStr += hex;
+//	}
+//
+//	return hashedStr;
+//}
+//
+//bool Security::VerifyPassword(std::string password, std::string hashedPassword, std::string salt)
+//{
+//	if (hashedPassword != HashingPassword(password, salt)) {
+//		return false;
+//	}
+//
+//	return true;
+//}
--- Server/Server/Source/DB/Security/Security.h
+++ Server/Server/Source/DB/Security/Security.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "../../utility.h"
-#include <openssl/sha.h>
-#include <openssl/rand.h>
-
-constexpr int SALT_LENGTH = 10;
-
-class Security {
-public:
-	std::string GenerateSalt();
-
-	std::string HashingPassword(const std::string password, const std::string salt);
-
-	bool VerifyPassword(std::string password, std::string hashedPassword, std::string salt);
-
-private:
-};
+//#include <openssl/sha.h>
+//#include <openssl/rand.h>
+//
+//constexpr int SALT_LENGTH = 10;
+//
+//class Security {
+//public:
+//	std::string GenerateSalt();
+//
+//	std::string HashingPassword(const std::string password, const std::string salt);
+//
+//	bool VerifyPassword(std::string password, std::string hashedPassword, std::string salt);
+//
+//private:
+//};
