a792754cc7908b69a545f00c9ca248ddafc9f501
sadending82 <sadending82@gmail.com>
2024-09-18 14:14:33 +0900

    World Space 관련 UI 매니저 코드 추가
    World Space로 띄워야 할 UI를 위해 코드 추가

--- Client/Assets/02. Scripts/Client/Managers/UIManager.cs
+++ Client/Assets/02. Scripts/Client/Managers/UIManager.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class UIManager
@@ -111,6 +112,22 @@ public class UIManager
         {
             ClosePopUpUI();
         }
+    }
+
+    public T MakeWorldSpace<T>(Transform parent = null, string name = null) where T : UI_Base
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"UI/WorldSpace/{name}");
+        if (parent != null)
+            go.transform.SetParent(parent);
+
+        Canvas canvas = go.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Camera.main;
+
+        return Util.GetOrAddComponent<T>(go);
     }
 
     public void Clear()
