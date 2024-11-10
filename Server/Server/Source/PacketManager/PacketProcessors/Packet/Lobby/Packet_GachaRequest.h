0252838a3c846a19ff626a46d4493347c80c5cc0
SeongJae Han <anis15740@gmail.com>
2024-11-10 17:40:28 +0900

    Update Packet_GachaRequest.h

--- Server/Server/Source/PacketManager/PacketProcessors/Packet/Lobby/Packet_GachaRequest.h
+++ Server/Server/Source/PacketManager/PacketProcessors/Packet/Lobby/Packet_GachaRequest.h
@@ -83,6 +83,11 @@ public:
 							db->UpsertUserItemCount(uid, rewardItem, rewardValue);
 							db->UpsertUserItemCount(uid, pay_item[i], -pay_Price[i]);
 							gacha_result = true;
+
+							usedItemCode = pay_item[i];
+							usedItemAmount = pay_Price[i];
+							usedItemRemaining = currPayItemAmount - usedItemAmount;
+
 							break;
 						}
 						else {
