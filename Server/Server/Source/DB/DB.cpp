c5a521dce495e2652fa7e9c4709e849e13fdb4b7
SeongJae Han <anis15740@gmail.com>
2024-11-12 13:51:05 +0900

    Revert "Server: 로그인 오류 수정"
    This reverts commit f6c6f00c8e09e20295ac78cef3a0becc1c2c8023.

--- Server/Server/Source/DB/DB.cpp
+++ Server/Server/Source/DB/DB.cpp
@@ -293,10 +293,7 @@ std::pair<bool, UserInfo> DB::SelectUserInfoForLogin(const char* id)
 
 	retcode = SQLExecute(hStmt);
 
-	SQLLEN rowCount;
-	SQLRowCount(hStmt, &rowCount);
-
-	if (rowCount == 1 && (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)) {
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
 
 		SQLLEN col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11;
 		TIMESTAMP_STRUCT date{};
