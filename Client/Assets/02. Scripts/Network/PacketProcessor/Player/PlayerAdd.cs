b0fc01abeab31895e43172c90d6b3c378f1d0f7b
SeongJae Han <anis15740@gmail.com>
2024-11-18 17:29:12 +0900

    Update PlayerAdd.cs

--- Client/Assets/02. Scripts/Network/PacketProcessor/Player/PlayerAdd.cs
+++ Client/Assets/02. Scripts/Network/PacketProcessor/Player/PlayerAdd.cs
@@ -26,6 +26,18 @@ namespace Network.PacketProcessor
                 int characterType = player.Value.CharacterType;
                 string nickname = player.Value.Nickname;
 
+                var characterCustomizing = player.Value.CharacterCustomizing.Value;
+
+                for (int j = 0; j < characterCustomizing.CustomizingItemsLength; j++)
+                {
+                    var item = characterCustomizing.CustomizingItems(i).Value;
+
+                    int item_UID = item.ItemUid;
+                    int item_Code = item.ItemCode;
+                    int item_Type = item.Type;
+                    
+                }
+
                 Vector3 pos = new Vector3(player.Value.Pos.Value.X, player.Value.Pos.Value.Y, player.Value.Pos.Value.Z);
                 Managers.Player.GetComponent<PlayerManager>().AddPlayer(id, pos, team);
             }
