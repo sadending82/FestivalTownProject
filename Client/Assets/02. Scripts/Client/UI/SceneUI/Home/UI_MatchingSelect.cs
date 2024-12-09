cc0dd5a7d132dc64eb25960348bb0cd457f40307
sadending82 <sadending82@gmail.com>
2024-12-09 16:42:47 +0900

    맵 코드 입력 기능 구현
    아직 서버의 패킷에서 맵 코드를 담는 기능이 없으므로 구현은 되어있지 않음.

--- Client/Assets/02. Scripts/Client/UI/SceneUI/Home/UI_MatchingSelect.cs
+++ Client/Assets/02. Scripts/Client/UI/SceneUI/Home/UI_MatchingSelect.cs
@@ -1,7 +1,10 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_MatchingSelect: UI_PopUp
 {
@@ -14,6 +17,7 @@ public class UI_MatchingSelect: UI_PopUp
         TutorialButton,
         TestMatchingButton,
         ExitButton,
+        MapCodeInputField,
     }
 
     // Start is called before the first frame update
@@ -28,9 +32,12 @@ public class UI_MatchingSelect: UI_PopUp
 
         Bind<GameObject>(typeof(GameObjects));
 
-
         // 치트가 활성화 되어 있지 않을 때에는 없애주기
-        if (false == Managers.Cheat.IsEnable()) Get<GameObject>((int)GameObjects.TestMatchingButton).SetActive(false);
+        if (false == Managers.Cheat.IsEnable())
+        {
+            Get<GameObject>((int)GameObjects.TestMatchingButton).SetActive(false);
+            Get<GameObject>((int)GameObjects.MapCodeInputField).SetActive(false);
+        }
 
         Get<GameObject>((int)GameObjects.IndivisualButton).BindEvent((PointerEventData) =>
         {
@@ -44,11 +51,26 @@ public class UI_MatchingSelect: UI_PopUp
             Managers.UI.ShowPopUpUI<UI_MatchingProgress>();
         });
 
-
         // 테스트용 매칭
         Get<GameObject>((int)GameObjects.TestMatchingButton).BindEvent((PointerEventData) =>
         {
-            Managers.Network.GetPacketManager().SendGameMatchingRequest(NetworkProtocol.eMatchingType.FITH_TEST);
+            string str = "";
+            if(Managers.Cheat.IsEnable())
+            {
+                str = Get<GameObject>((int)GameObjects.MapCodeInputField).GetComponent<TMP_InputField>().text;
+            }
+
+            string fileStr = Application.streamingAssetsPath + $"/Map/{str}.txt";
+            if (File.Exists(fileStr))
+            {
+                Debug.Log("Map True");
+                Managers.Network.GetPacketManager().SendGameMatchingRequest(NetworkProtocol.eMatchingType.FITH_TEST);
+            }
+            else
+            {
+                Debug.Log("Map False");
+                Managers.Network.GetPacketManager().SendGameMatchingRequest(NetworkProtocol.eMatchingType.FITH_TEST);
+            }
             Managers.UI.ShowPopUpUI<UI_MatchingProgress>();
         });
 
@@ -56,5 +78,7 @@ public class UI_MatchingSelect: UI_PopUp
         {
             ClosePopUpUI();
         });
+
+
     }
 }
