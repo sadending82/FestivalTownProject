376d1134449fc5147bff89f88c8b5d144238a14c
SeongJae Han <anis15740@gmail.com>
2024-12-25 16:54:15 +0900

    Update NetworkSelector.cs

--- Client/Assets/02. Scripts/Network/NetworkSelector.cs
+++ Client/Assets/02. Scripts/Network/NetworkSelector.cs
@@ -66,7 +66,8 @@ public class NetworkSelect : EditorWindow
                 break;
             case 2:
                 IPAddressObj.IPAddress = "203.239.231.148";
-                PortNumberObj.PortNumber = "30412";
+                //PortNumberObj.PortNumber = "30412";
+                PortNumberObj.PortNumber = "45872";
                 break;
             case 3:
                 IPAddressObj.IPAddress = _ipAddressText;
