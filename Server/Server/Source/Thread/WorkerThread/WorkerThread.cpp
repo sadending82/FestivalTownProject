03b847ffdc4e069c457408aa206359e0364f3ef0
seonghj <anis15740@gmail.com>
2025-01-13 16:47:30 +0900

    Update WorkerThread.cpp

--- Server/Server/Source/Thread/WorkerThread/WorkerThread.cpp
+++ Server/Server/Source/Thread/WorkerThread/WorkerThread.cpp
@@ -3,6 +3,8 @@
 #include "../../PacketManager/PacketManager.h"
 #include <mstcpip.h>
 
+inline constexpr int PACKET_HEADER_SIZE = sizeof(HEADER);
+
 WorkerThread::~WorkerThread()
 {
 
@@ -31,19 +33,6 @@ void WorkerThread::RunWorker()
                 Session* newSession = pServer->GetSessions()[newKey];
                 SOCKET cSocket = reinterpret_cast<SOCKET>(exOver->mWsaBuf.buf);
                 newSession->SessionInit(cSocket, newKey);
-
-                //// Keep-Alive 활성화
-                //BOOL keepAlive = TRUE;
-                //setsockopt(cSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&keepAlive, sizeof(keepAlive));
-
-                //// 상세 Keep-Alive 설정
-                //tcp_keepalive kaSettings;
-                //kaSettings.onoff = 1;
-                //kaSettings.keepalivetime = 5000;
-                //kaSettings.keepaliveinterval = 5000;
-
-                //DWORD bytesReturned;
-                //WSAIoctl(cSocket, SIO_KEEPALIVE_VALS, &kaSettings, sizeof(kaSettings), NULL, 0, &bytesReturned, NULL, NULL);
 
                 CreateIoCompletionPort((HANDLE)newSession->GetSocket(), pServer->GetHcp(), newKey, 0);
                 DEBUGMSGONEPARAM("Lobby Accept: %d\n", newKey);
@@ -83,31 +72,30 @@ void WorkerThread::RunWorker()
             unsigned char* packet_ptr = exOver->GetMessageBuf();
 
             // 이전 작업에서 남은 데이터와 현재 받은 데이터를 재조립한 후 데이터 크기
-            int pendingData = Transferred + session->GetPrevDataSize();
+            int pendingDataSize = Transferred + session->GetPrevDataSize();
 
             // 헤더의 버퍼 크기 정보만 읽어서 packetSize 확인
             uint16_t BufSize = 0;
             std::memcpy(&BufSize, packet_ptr, sizeof(uint16_t));
-            const int headerSize = sizeof(HEADER);
-            int packetSize = BufSize + headerSize;
+            int packetSize = BufSize + PACKET_HEADER_SIZE;
 
             // 헤더의 정보를 바탕으로 순차적으로 데이터 처리
-            while (pendingData >= packetSize) {
-                if (pendingData >= BUFSIZE) break;
+            while (pendingDataSize >= packetSize) {
+                if (pendingDataSize >= BUFSIZE) break;
                 if (packetSize <= 0) break;
       
                 pPacketManager->ProcessPacket(packet_ptr, key);
-                pendingData -= packetSize;
+                pendingDataSize -= packetSize;
                 packet_ptr += packetSize;
 
                 // 처리 후 남은 데이터의 헤더를 읽어 packetSize 갱신
                 uint16_t nextBufSize = 0;
                 std::memcpy(&nextBufSize, packet_ptr, sizeof(uint16_t));
-                packetSize = nextBufSize + headerSize;
+                packetSize = nextBufSize + PACKET_HEADER_SIZE;
             }
-            session->SetPrevDataSize(pendingData);
-            if (0 != pendingData) {
-                memcpy(exOver->mMessageBuf, packet_ptr, pendingData);
+            session->SetPrevDataSize(pendingDataSize);
+            if (0 != pendingDataSize) {
+                memcpy(exOver->mMessageBuf, packet_ptr, pendingDataSize);
             }
             session->DoRecv();
             break;
